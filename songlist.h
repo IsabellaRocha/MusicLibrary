3ebbeb709832c675a873cb7e6dafc455467f7cad
Isabella Rocha <irocha00@stuy.edu>
2019-10-29 15:29:26 -0400

    Added headers for node functions

---
+++ songlist.h
@@ -0,0 +1,13 @@
+struct song_node{
+    char name[100];
+    char artist[100];
+    struct song_node *next;
+};
+
+struct song_node * insert_front(struct song_node * n, char Name[100], char Artist[100]);
+struct song_node * insert_alph(struct song_node * n, char Name[100], char Artist[100]);
+void print_list(struct song_node * n);
+struct song_node * find_song( char Name[100], char Artist[100]);
+struct song_node * find_first_song( char Artist[100]);
+struct song_node * rando(struct song_node * n);
+struct song_node *free_list(struct song_node * n);
