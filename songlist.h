5ac0472b7e7cd4783256670adc15c23bf70b969a
Isabella Rocha <irocha00@stuy.edu>
2019-10-31 15:09:38 -0400

    Edited two of the functions headers

--- songlist.h
+++ songlist.h
@@ -7,7 +7,7 @@ struct song_node{
 struct song_node * insert_front(struct song_node * n, char Name[100], char Artist[100]);
 struct song_node * insert_alph(struct song_node * n, char Name[100], char Artist[100]);
 void print_list(struct song_node * n);
-struct song_node * find_song( char Name[100], char Artist[100]);
-struct song_node * find_first_song( char Artist[100]);
+struct song_node * find_song(struct song_node * n, char Name[100], char Artist[100]);
+struct song_node * find_first_song(struct song_node * n, char Artist[100]);
 struct song_node * rando(struct song_node * n);
 struct song_node *free_list(struct song_node * n);
